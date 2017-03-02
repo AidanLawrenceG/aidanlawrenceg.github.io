5d00242e7784fb10db69d5ca879f9e818f53101a
AidanLawrenceG <aidan.lawrence1303@gmail.com>
2017-03-02 10:38:10 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,7 +13,6 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-<<<<<<< HEAD
 
   // slider = createSlider(0, 10, 1);
   // slider.position(20, 350);
@@ -25,12 +24,7 @@ function setup() {
 
   points  = font.textToPoints(random(words), 50, 300, 300);
   gravity = createVector(0, 2);
-=======
->>>>>>> origin/master
 
-  var points  = font.textToPoints(random(words), 50, 300, 300);
-  gravity = createVector(0, 2);
-    
   for(var i = 0; i < points.length; i++){
     var pt = points[i];
     var vehicle = new Vehicle(pt.x, pt.y);
